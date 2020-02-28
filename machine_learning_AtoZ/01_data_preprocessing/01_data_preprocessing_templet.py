d08127e8232a6cec61d2bda82a2a4cef8f9f938a
lhs7091 <lhs7091@naver.com>
2020-02-28 21:51:49 +0900

    #1 data preprocessing
    by https://www.superdatascience.com/pages/machine-learning
    # Splitting the dataset into the Training set and Test set

--- machine_learning_AtoZ/01_data_preprocessing/01_data_preprocessing_templet.py
+++ machine_learning_AtoZ/01_data_preprocessing/01_data_preprocessing_templet.py
@@ -1,16 +1,21 @@
 # Data Preprocessing
 
-# Import the libraries
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.impute import SimpleImputer
 
 # Importing the dataset
 dataset = pd.read_csv('./data/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
-print(y)
+
+# Taking care of missing data
+# Updated Imputer
+from sklearn.impute import SimpleImputer
+missingvalues = SimpleImputer(missing_values = np.nan, strategy = 'mean', verbose = 0)
+missingvalues = missingvalues.fit(X[:, 1:3])
+X[:, 1:3]=missingvalues.transform(X[:, 1:3])
 
 # Taking care of missing data
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
@@ -18,6 +23,25 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 print(X)
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+# Encoding Y data
+from sklearn.preprocessing import LabelEncoder
+y = LabelEncoder().fit_transform(y)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+print(X_train)
+
 
 
 
